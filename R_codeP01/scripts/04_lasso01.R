2e84f2c251dc77366b544a416234b2845bcb5999
Anders Kielland <anders@eduroam-193-157-200-70.wlan.uio.no>
2022-09-28 18:57:44 +0200

    write lasso code with boosting

--- R_codeP01/scripts/04_lasso01.R
+++ R_codeP01/scripts/04_lasso01.R
@@ -1,7 +1,10 @@
 
+X <- dplyr::select(df02_LR,  scr_CCND1, scr_CCNE1, scr_CDKN1A, scr_ESR1, scr_MYC, scr_RB1)
+Y <- select(df02_LR, sur_ProliferationScore)
+
 
 # Leave one observation out for testing and going through all observations
-lasso_loo <- function(X, Y, lambda){
+lasso_loo <- function(X, Y, lambda.min=TRUE){
   X = as.matrix(X)
   Y = as.matrix(Y)
   n = length(Y)
@@ -9,59 +12,81 @@ lasso_loo <- function(X, Y, lambda){
   for(i in 1:nrow(X)){
     X_test <- X[i,]
     X_train <- X[-i,]
-    Y_test <- Y[i,]
+    # Y_test <- Y[i,]
     Y_train <- Y[-i,]
     
     lasso.cv <- cv.glmnet(X_train, Y_train)
-    if (lambda == one_se) {
-      pred_values[i] = predict(lasso.cv, newx = X_test, type = "response", s = "lambda.1se")      
-    }
-    if (lambda = min) {
+
+    if (lambda.min == TRUE) {
       pred_values[i] = predict(lasso.cv, newx = X_test, type = "response", s = "lambda.min")      
+    }
+    if (lambda.min == FALSE) {
+      pred_values[i] = predict(lasso.cv, newx = X_test, type = "response", s = "lambda.1se")      
     }
   }
   return(pred_values)
 }
 
-X <- dplyr::select(df02_LR,  scr_CCND1, scr_CCNE1, scr_CDKN1A, scr_ESR1, scr_MYC, scr_RB1)
-Y <- select(df02_LR, sur_ProliferationScore)
+pred_values_lasso.min <- lasso_loo(X, Y, TRUE)
+pred_values_lasso.1se <- lasso_loo(X, Y, FALSE)
 
-pred_values_lasso <- lasso_loo(X, Y, min)
-
-# Result of simple linear model:
+# Result of lasso with lambda at min:
 par(mfrow=c(1,2))
 cat("Correlation for lasso using lambda.min: ")
-print(cor(pred_values_lasso, df02_LR$sur_ProliferationScore))
+print(cor(pred_values_lasso.min, df02_LR$sur_ProliferationScore))
 cat("MSE for lasso using lambda.min: ")
-print(mean((pred_values_lasso - df02_LR$sur_ProliferationScore)**2))
-plot(pred_values_lasso, df02_LR$sur_ProliferationScore, main = "Lasso")
+print(mean((pred_values_lasso.min - df02_LR$sur_ProliferationScore)**2))
+plot(pred_values_lasso.min, df02_LR$sur_ProliferationScore, main = "Lasso")
 
-# Result of linear model with interactions:
-cat("Correlation for linear model with interactions: ")
-print(cor(pred_values_fm02, df02_LR$sur_ProliferationScore))
-cat("MSE for linear model with interactions: ")
-print(mean((pred_values_fm02 - df02_LR$sur_ProliferationScore)**2))
-plot(pred_values_fm02, df02_LR$sur_ProliferationScore, main = "Linear model with interaction terms")
+# Result of lasso wih lambda at 1 se from min:
+cat("Correlation for lasso using lambda.min: ")
+print(cor(pred_values_lasso.1se, df02_LR$sur_ProliferationScore))
+cat("MSE for lasso using lambda.min: ")
+print(mean((pred_values_lasso.1se - df02_LR$sur_ProliferationScore)**2))
+plot(pred_values_lasso.1se, df02_LR$sur_ProliferationScore, main = "Lasso")
 par(mfrow=c(1,1))
 
+######################################################################
 
-pred.lasso.loop = function(X, Y){
-  df_ <- data.frame(matrix(ncol = 2, nrow = 0))
-  colnames(df_) <- c("lambda.min", "Y_test")
+# boostrap
+lasso_bootstrap <- function(X, Y, lambda.min=TRUE){
+  X = as.matrix(X)
+  Y = as.matrix(Y)
+  N = length(Y)
+
+  int <- sample(N, size = N*0.632, replace = TRUE)
+  X_train = X[int,]
+  X_test = X[-int,]
+  Y_train = Y[int]
+  Y_test = Y[-int]
   
-  for (i in c(1:10)) {
-    df_ = rbind(df_, prediction.lasso(X,Y))
-  }  
-  return(df_)
+  lasso.cv <- cv.glmnet(X_train, Y_train)
+  
+  if (lambda.min == TRUE) {
+    pred_values = predict(lasso.cv, newx = X_test, type = "response", s = "lambda.min")      
+  }
+  if (lambda.min == FALSE) {
+    pred_values = predict(lasso.cv, newx = X_test, type = "response", s = "lambda.1se")      
+  }
+  suppressWarnings(out <- cor(pred_values, Y_test))
+  return(out)
 }
 
-pip.test.l = pred.lasso.loop(X.sim.df, y)
+lasso_bootstrap(X, Y, TRUE)
 
-## Results: Lasso prediction
+lasso_cor_boot = function(X, Y, n_bootstraps){
+  cor_vec <- rep(NA, n_bootstraps)
+  for (i in c(1:n_bootstraps)) {
+    cor_vec[i] = lasso_bootstrap(X, Y, TRUE)
+  }  
+  return(cor_vec)
+}
 
-plot(pip.test.l[,1], pip.test.l[,2])
-abline(lm(pip.test.l[,2] ~ pip.test.l[,1]), col = "red", lwd=3)
-mtext(paste("Correlation:", round(cor(pip.test.l[,2], pip.test.l[,1]), 3)), side=1, line=-1, cex=1.4)
+cor_vec_boot <- lasso_cor_boot(X,Y,1000)
+sum(is.na(cor_vec_boot))/1000
+mean(cor_vec_boot, na.rm=TRUE)
+var(cor_vec_boot, na.rm=TRUE)
+hist(cor_vec_boot, breaks = 50)
 
-mean((pip.test.l[,1] - pip.test.l[,2])^2)
+
 
