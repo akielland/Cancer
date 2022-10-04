18db1cd5f4b6acb4ac1e4f3e870d7060a598c43d
Anders Kielland <43873625+akielland@users.noreply.github.com>
2022-10-04 14:49:31 +0200

    extracting coef from lasso

--- R_codeP01/scripts/04_lasso01.R
+++ R_codeP01/scripts/04_lasso01.R
@@ -1,20 +1,27 @@
 
+# X and Y from the 6 gene data
 X <- dplyr::select(df02_LR,  scr_CCND1, scr_CCNE1, scr_CDKN1A, scr_ESR1, scr_MYC, scr_RB1)
 Y <- select(df02_LR, sur_ProliferationScore)
 
+# X from the full data set
 X <- select(df04, -"Proliferation.Score") |> 
   select(-"Ki67")
 
+# Y's from the full data set
 Y <- select(df04, "Proliferation.Score")
-
+Y <- select(df04, "Ki67")
+Y[is.na(Y)] = 0
 
 lasso01 <- glmnet(as.matrix(X), as.matrix(Y))
 plot(lasso01, label = T)
-print(lasso01)
-coef(lasso01, s = lambda.min)
+co <- coef(lasso01, s = 1)
 
-fit = glmnet(as.matrix(X), as.matrix(Y))
-test = coef(fit, s = cv.glmnet(as.matrix(X), as.matrix(Y))$lambda.1se)
+lasso02 = cv.glmnet(as.matrix(X), as.matrix(Y), nfolds=5)
+co <- coef(lasso02, s = "lambda.min")
+inds <- which(co!=0)
+variables <- row.names(co)[inds]
+variables <- variables[!(variables %in% '(Intercept)')];
+
 
 # Leave one observation out for testing and going through all observations
 lasso_loo <- function(X, Y, lambda.min=TRUE){
@@ -53,7 +60,7 @@ print(mean((pred_values_lasso.min - PS)**2))
 plot(pred_values_lasso.min, PS, main = "Lasso")
 abline(lm(PS ~ pred_values_lasso.min), pred_values_lasso.min)
 
-# Result of lasso wih lambda at 1 se from min:
+# Result of lasso with lambda at 1 se from min:
 cat("Correlation for lasso using lambda.min: ")
 print(cor(pred_values_lasso.1se, PS))
 cat("MSE for lasso using lambda.min: ")
@@ -61,9 +68,12 @@ print(mean((pred_values_lasso.1se - PS)**2))
 plot(pred_values_lasso.1se, PS, main = "Lasso")
 
 ######################################################################
+##  bootstrap  ##
+#################
 
-# boostrap
-lasso_bootstrap <- function(X, Y, lambda.min=TRUE){
+lasso_bootstrap_sample <- function(X, Y, lambda.min=TRUE){
+  # one bootstrap sample
+  # output: correlation between prediction and true of the leave-out of the bootstrap sample
   X = as.matrix(X)
   Y = as.matrix(Y)
   N = length(Y)
@@ -74,24 +84,35 @@ lasso_bootstrap <- function(X, Y, lambda.min=TRUE){
   Y_train = Y[int]
   Y_test = Y[-int]
   
-  lasso.cv <- cv.glmnet(X_train, Y_train)
+  lasso.cv <- cv.glmnet(X_train, Y_train, nfolds = 5)
   
+  co <- coef(lasso.cv, s = "lambda.min")
+  inds <- which(co!=0)
+  variables <- row.names(co)[inds]
+  variables <- variables[!(variables %in% '(Intercept)')];
+    
   if (lambda.min == TRUE) {
     pred_values = predict(lasso.cv, newx = X_test, type = "response", s = "lambda.min")      
   }
   if (lambda.min == FALSE) {
     pred_values = predict(lasso.cv, newx = X_test, type = "response", s = "lambda.1se")      
   }
-  suppressWarnings(out <- cor(pred_values, Y_test))
-  return(out)
+  cor <- suppressWarnings(cor(pred_values, Y_test))
+  return(c(cor, variables))
 }
 
-lasso_bootstrap(X, Y, TRUE)
+lasso_bootstrap_sample(X, Y, TRUE)
 
 lasso_cor_boot = function(X, Y, n_bootstraps){
+  # run many boostraps
+  # output: vector with correlations
   cor_vec <- rep(NA, n_bootstraps)
+  #var_vec <- data_frame(NA, n_bootstraps)
+  
   for (i in c(1:n_bootstraps)) {
-    cor_vec[i] = lasso_bootstrap(X, Y, TRUE)
+    cor_var <- lasso_bootstrap_sample(X, Y, TRUE)
+    cor_vec[i] = cor_var[1]
+    #var_vec[i] = as.list(cor_var[-1])
   }  
   return(cor_vec)
 }
