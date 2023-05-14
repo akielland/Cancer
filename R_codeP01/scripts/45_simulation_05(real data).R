6f6e987a040ea4fecf25275d950974b80c989100
Anders Kielland <43873625+akielland@users.noreply.github.com>
2023-05-14 08:11:43 +0200

    syn trial

--- R_codeP01/scripts/45_simulation_05(real data).R
+++ R_codeP01/scripts/45_simulation_05(real data).R
@@ -4,77 +4,9 @@
 
 library(glmnet)
 
-# Function that creates a synthetic dataset (matrix) with interaction effects
-syntetic_01 <- function(n, interact, p_gr, p_non_gr, i, scale=TRUE){
-  # Create predictors for groups
-  X <- matrix(rnorm(n * p_gr), n, p_gr)
-  colnames(X) <- paste0("X", 1:p_gr)
-  # Add non-gr. predictors
-  Z <- matrix(rnorm(n * p_non_gr), nrow = n)
-  X <- cbind(X, Z)
-  colnames(X)[(p_gr + 1):(p_gr + p_non_gr)] <- paste0("Z", 1:p_non_gr)
-  # X_df <- as.data.frame(X)
-
-  
-  # Define character lists for each group of features
-  char_group1 <- colnames(X)[1:5]
-  char_group2 <- colnames(X)[6:10]
-  char_group3 <- colnames(X)[11:15]
-  char_group4 <- colnames(X)[16:20]
-  
-  # Create matrices w/ groups of predictors -> predictors also divided into groups
-  X_group1 <- X[, char_group1]
-  X_group2 <- X[, char_group2]
-  X_group3 <- X[, char_group3]
-  X_group4 <- X[, char_group4]
-  
-  char_list_sim <- list(gr1 = char_group1,
-                        gr2 = char_group2,
-                        gr3 = char_group3,
-                        gr4 = char_group4)
-  
-  # Create true betas
-  true_betas <- runif(p_gr, -1, 1)
-  # Add non effective betas = 0
-  betas <- c(true_betas, rep(0, p_non_gr))
-  
-  # divide betas into groups
-  beta1 <- matrix(true_betas[1:5], ncol=1)
-  beta2 <- matrix(true_betas[6:10], ncol=1)
-  beta3 <- matrix(true_betas[11:15], ncol=1)
-  beta4 <- matrix(true_betas[16:20], ncol=1)
-  
-
-  if (interact == 1){
-    # Add interaction effects (1): gr1 x gr2.  -> 1 interaction term between the 4 groups: gives 6 simulations
-    interaction_effect <- (X_group1 %*% beta1) * (X_group2 %*% beta2) * 1
-  } else if (interact == 2) {
-    # Add interaction effects (2): gr1 x gr2 + gr1 x gr4
-    interaction_effect <- (X_group1 %*% beta1) * (X_group2 %*% beta2) * 1 + (X_group1 %*% beta1) * (X_group4 %*% beta4) * 1
-  } else if (interact == 3){
-    # Add interaction effects (3): gr1 x gr2 + gr1 x gr4 + gr2 x gr4
-    interaction_effect <- (X_group1 %*% beta1) * (X_group2 %*% beta2) * 1 + (X_group1 %*% beta1) * (X_group4 %*% beta4) * 1 + (X_group2 %*% beta2) * (X_group4 %*% beta4) * 1
-  }else {
-    abort("Error: interact argument must be 1, 2, or 3.")
-  }
-    
-
-  # use model to create response y
-  y <- X %*% betas + interaction_effect + rnorm(n)
-  colnames(y) <- "y"
-  
-  if (scale==TRUE)
-    X <- scale(X)
-    y <- scale(y)
-  
-  df <- as.data.frame(cbind(y,X))  
-  return(list(df.sim=df, char_list=char_list_sim))
-}
-
-
 
 # Function: repeated k-fold cross validation: 
-simulation_rc <- function(df_data, alpha, groups, folds=5, repeats=2, method="pearson") {
+real_data_rc <- function(df_data, alpha, groups, folds=5, repeats=2, method="pearson") {
   n_models <- repeats * folds
   print(n_models)
   
@@ -125,53 +57,8 @@ simulation_rc <- function(df_data, alpha, groups, folds=5, repeats=2, method="pe
 
 
 
+real_t1 <- 
 
-# Function to run many simulations
-run_sim_02 <- function(n_simulations, interact, alpha, adaptive, tol){
-
-  coef_matrix <- matrix(NA, nrow = n_simulations, ncol = 6)
-  correlation_mean <- rep(NA, n_simulations)
-  MSE_mean <- rep(NA, n_simulations)
-  
-  row_index <- 1
-  for (i in c(1:n_simulations)){
-    # i = i+12345
-    set.seed(i) 
-    
-    data <- syntetic_01(n, interact, p_gr, p_non_gr, i)
-    
-    results <- simulation_rc(df_data=data$df.sim, alpha, data$char_list)
-    
-    # print(out$beta_interaction)
-
-    #coef_matrix[row_index, ] <- results$coef_matrix
-    cat(row_index, "")
-    row_index <- row_index + 1
-    
-    correlation_mean[i] <- results$cor_mean
-    MSE_mean[i] <- results$MSE_mean
-  }
-  colnames(coef_matrix) <- c("1x2", "1x3", "1x4", "2x3", "2x4", "3x4")
-  return(list(cor_mean = correlation_mean, MSE_mean = MSE_mean, coef_matrix=coef_matrix))
-}
-
-
-
-
-n <- 50
-n <- 100
-n <- 500
-
-p_gr <- 20
-p_non_gr <- 100
-interact <- 1
-interact <- 2
-interact <- 3
-
-n_simulations <- 100
-
-t1 <- run_sim_02(n_simulations, interact, alpha = 1, adaptive = FALSE, tol=1e-4)
-t2 <- run_sim_02(n_simulations, interact, alpha = 0.5, adaptive = FALSE, tol=1e-6)
 
 mean(t2$cor_mean, na.rm=T)
 sd(t2$cor_mean, na.rm=T)
@@ -189,54 +76,7 @@ syn_sim_rc_50n_interact1 <- run_sim_02(n_simulations, interact, alpha = 0.5, ada
 save(syn_sim_rc_50n_interact1, file="/Users/anders/Documents/MASTER/Cancer/R_codeP01/instances/syn_sim_rc_50n_interact1.RData")
 load("/Users/anders/Documents/MASTER/Cancer/R_codeP01/instances/syn_sim_rc_50n_interact1.RData")
 mean(syn_sim_rc_50n_interact1$cor_mean, na.rm=T)
-sd(syn_sim_rc_50n_interact1$cor_mean, na.rm=T)
-
 mean(syn_sim_rc_50n_interact1$MSE_mean)
-sd(syn_sim_rc_50n_interact1$MSE_mean)
-
-syn_sim_rc_100n_interact1 <- run_sim_02(n_simulations, interact, alpha = 0.5, adaptive = FALSE, tol=1e-6)
-save(syn_sim_rc_100n_interact1, file="/Users/anders/Documents/MASTER/Cancer/R_codeP01/instances/syn_sim_rc_100n_interact1.RData")
-load("/Users/anders/Documents/MASTER/Cancer/R_codeP01/instances/syn_sim_rc_100n_interact1.RData")
-mean(syn_sim_rc_100n_interact1$cor_mean, na.rm=T)
-sd(syn_sim_rc_100n_interact1$cor_mean, na.rm=T)
-
-mean(syn_sim_rc_100n_interact1$MSE_mean)
-sd(syn_sim_rc_100n_interact1$MSE_mean)
-
-syn_sim_rc_500n_interact1 <- run_sim_02(n_simulations, interact=1, alpha = 0.5, adaptive = FALSE, tol=1e-6)
-save(syn_sim_rc_500n_interact1, file="/Users/anders/Documents/MASTER/Cancer/R_codeP01/instances/syn_sim_rc_500n_interact1.RData")
-load("/Users/anders/Documents/MASTER/Cancer/R_codeP01/instances/syn_sim_rc_500n_interact1.RData")
-mean(syn_sim_rc_500n_interact1$cor_mean, na.rm=T)
-sd(syn_sim_rc_500n_interact1$cor_mean, na.rm=T)
-
-mean(syn_sim_rc_500n_interact1$MSE_mean)
-sd(syn_sim_rc_500n_interact1$MSE_mean)
-
-
-syn_sim_rc_50n_interact3 <- run_sim_02(n_simulations, interact=3, alpha = 0.5, adaptive = FALSE, tol=1e-6)
-syn_sim_rc_50n_interact3$cor_mean
-save(syn_sim_rc_50n_interact3, file="/Users/anders/Documents/MASTER/Cancer/R_codeP01/instances/syn_sim_rc_50n_interact3.RData")
-load("/Users/anders/Documents/MASTER/Cancer/R_codeP01/instances/syn_sim_rc_50n_interact3.RData")
-mean(syn_sim_rc_50n_interact3$cor_mean, na.rm=T)
-mean(syn_sim_rc_50n_interact3$MSE_mean)
-
-syn_sim_rc_100n_interact3 <- run_sim_02(n_simulations, interact=3, alpha = 0.5, adaptive = FALSE, tol=1e-6)
-save(syn_sim_rc_100n_interact3, file="/Users/anders/Documents/MASTER/Cancer/R_codeP01/instances/syn_sim_rc_100n_interact3.RData")
-load("/Users/anders/Documents/MASTER/Cancer/R_codeP01/instances/syn_sim_rc_100n_interact3.RData")
-mean(syn_sim_rc_100n_interact3$cor_mean, na.rm=T)
-sd(syn_sim_rc_100n_interact3$cor_mean, na.rm=T)
-
-mean(syn_sim_rc_100n_interact3$MSE_mean)
-sd(syn_sim_rc_100n_interact3$MSE_mean)
-
-syn_sim_rc_500n_interact3 <- run_sim_02(n_simulations, interact=3, alpha = 0.5, adaptive = FALSE, tol=1e-6)
-save(syn_sim_rc_500n_interact3, file="/Users/anders/Documents/MASTER/Cancer/R_codeP01/instances/syn_sim_rc_500n_interact3.RData")
-load("/Users/anders/Documents/MASTER/Cancer/R_codeP01/instances/syn_sim_rc_500n_interact3.RData")
-mean(syn_sim_rc_500n_interact3$cor_mean, na.rm=T)
-sd(syn_sim_rc_500n_interact3$cor_mean, na.rm=T)
-
-mean(syn_sim_rc_500n_interact3$MSE_mean)
-sd(syn_sim_rc_500n_interact3$MSE_mean)
 
 
 
@@ -382,20 +222,11 @@ synergistic_adaptive_deviance02 <- function(df, X_test, char_list, alpha = 0.5, 
 
 #result <- synergistic(ROR_prolif_771genes, char_list, alpha = 0.1, lambda_seq = NULL, nfolds = 5, tol = 1e-5, max_iters = 100)
 
-t1 <- run_sim_02(n_simulations, interact, alpha = 1, adaptive = FALSE, tol=1e-4)
 
 
 
 
 
-
-
-
-
-char_list_sim <- list(gr1 = char_group1,
-                      gr2 = char_group2,
-                      gr3 = char_group3,
-                      gr4 = char_group4)
 
 
 
